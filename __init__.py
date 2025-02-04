43dc14645ac2c43c2cb228d7d32bde500de28809
Andrey <andrejmarcenko334@gmail.com>
2025-02-04 16:04:15 +0700

    50/50 palevo

--- __init__.py
+++ __init__.py
@@ -3,3 +3,11 @@ from config import *
 
 app = Flask(__name__)
 app.config.from_object('config')
+from flask import Flask, request
+@app.route("/")
+def index():
+    ip = request.remote_addr
+    return f"Ваш IP-адрес: {ip}"
+
+if __name__ == "__main__":
+    app.run()
